81822ceaed7243eca46cc642d2289feedf1fd1d0
teodor <doru.dev.now@gmail.com>
2020-03-07 19:31:06 +0100

    add LicitatiiView component

--- src/views/LicitatiiView.vue
+++ src/views/LicitatiiView.vue
@@ -1,5 +1,8 @@
 <template>
-  <div><Home /></div>
+  <div>
+    <div class="headline text-center">Good Luck and Have Fun</div>
+    <Home />
+  </div>
 </template>
 
 <script lang="ts">
@@ -9,6 +12,11 @@ import Home from "@/components/Home.vue";
 export default Vue.extend({
   components: {
     Home
+  },
+  computed: {
+    type() {
+      return this.$route.path;
+    }
   }
 });
 </script>
