edf87e84af3a87b26c72ade6bd6769373124b027
teodor <doru.dev.now@gmail.com>
2020-03-09 07:33:04 +0100

    add socket integration

---
+++ src/components/Socket.vue
@@ -0,0 +1,28 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  sockets: {
+    connect() {
+      console.log("socket connected");
+    },
+    customEmit(val) {
+      console.log(
+        'this method was fired by the socket server. eg: io.emit("customEmit", data)'
+      );
+    }
+  },
+  methods: {
+    clickButton(val: string) {
+      // this.$socket.client is `socket.io-client` instance
+      this.$socket.client.emit("emit_method", val);
+    }
+  }
+});
+</script>
+
+<style scoped></style>
